a79f1e945421081094427d2e8a01ec13a9f9ec48
Anish Karim <anish@litmus7.com>
2021-01-02 06:49:52 +0000

    Finishing up the episode with script02

---
+++ S01E01/script02.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from time import sleep
+## Interesting things are coming up ##
+
